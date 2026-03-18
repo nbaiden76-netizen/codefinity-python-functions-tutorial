593f2a7dd641e08f9944d4bda220195d04c709ae
nbaiden76-netizen <>
2026-03-18 22:27:51 +0000

    Function Arguments

---
+++ what_is_a_function_in_python/function_arguments/main.py
@@ -0,0 +1,35 @@
+def calculate_triangle_perimeter(a, b, c):
+    # Calculate perimeter by summing all sides
+    perimeter = a + b + c
+    # Print the calculated perimeter
+    print(f'The perimeter is {perimeter}')
+
+#Testing the result
+calculate_triangle_perimeter(2, 3, 5)
+
+# Specify two arguments of the function
+#def sum_of_two_numbers(num_1, num_2):
+    # Use arguments to calculate sum
+#    result = num_1 + num_2
+    # Print the result
+#    print(f'Sum of two numbers is {result}')
+ 
+# Call the function 
+#sum_of_two_numbers(2, 10)
+
+# Define the function `calculate_list_sum`
+#def calculate_list_sum(list):
+    # Initialize the total sum to 0
+ #   total = 0
+    # Iterate through each number in the list
+#    for num in list:
+        # Add the current number to the total sum
+#        total += num
+    # Print the total sum
+#    print(total)
+
+# Create a list of numbers
+#my_list = [1, 2, 3, 4, 5]
+# Call the function 
+#calculate_list_sum(my_list)
+

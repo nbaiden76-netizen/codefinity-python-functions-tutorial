4dadcc28991ce10fd51d94004a45484a29a987b3
nbaiden76-netizen <>
2026-03-18 22:08:00 +0000

    Printing Functions

---
+++ what_is_a_function_in_python/printing_functions/main.py
@@ -0,0 +1,16 @@
+# Define the function
+your_name = "Nana Baiden"
+def print_name():
+
+    print(f'My name is', {your_name})
+
+# Call the function for testing  
+print_name()
+
+# Define the function `say_my_name`
+#def say_my_name():
+    # The function prints the string `My name is Heisenberg`
+#    print('My name is Heisenberg')
+
+# Call the function `say_my_name`
+#say_my_name()

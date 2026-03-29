0aa3a3373b9b28df797cd0b20098fc885377c590
nbaiden76-netizen <>
2026-03-29 16:48:06 +0000

    Lambda IIFE

---
+++ recursion_and_lambda_functions/lambda_iife/main.py
@@ -0,0 +1,12 @@
+celsius_temperature = 23
+
+# The `lambda` function for converting temperature 
+# from `celsius_temperature` variable to fahrenheit temperature 
+fahrenheit_temperature = (lambda celsius: 9 / 5 * celsius + 32 )(celsius_temperature)
+
+# Testing the result
+print(f"{celsius_temperature} degrees Celsius is equal to {fahrenheit_temperature} degrees Fahrenheit")
+
+#square = (lambda x: x**2)(5)
+#print(square)
+
